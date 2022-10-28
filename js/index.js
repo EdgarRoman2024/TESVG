544378e143e90f993ab97ef97c2bd838adb926e3
edgaraugusto.roman.contractor <edgaraugusto.roman.contractor@bbva.com>
2022-10-28 11:32:32 -0500

    Cambios 28/10/2022

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const d = document, 
+w = window;
+
+d.addEventListener('DOMContentLoaded', e=>{
+    scrollButton('.scroll-top-btn');
+});
+
+function scrollButton(btn) {
+    const $scrollBtn = d.querySelector(btn);
+    w.addEventListener('scroll', e =>{
+        let scrollTop = w.pageYOffset || d.documentElement.scrollTop;
+
+        if (scrollTop > 600) {
+           $scrollBtn.classList.remove("hidden");
+        }else{
+           $scrollBtn.classList.add("hidden");
+        }
+        // console.log(w.pageYOffset, d.documentElement.scrollTop);
+    });
+    d.addEventListener('click', e=>{
+        if (e.target.matches(btn)) {
+            w.scrollTo({
+              behavior: "smooth",
+              top: 0,  
+            });
+    }});
+
+}
