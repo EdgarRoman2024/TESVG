4a5e85fbec22816ba23df7903b584f6b71fd4b78
EdGaRoman1012 <edgar.pgc.estudiante@gmail.com>
2023-01-23 16:29:20 -0600

    cambio

--- js/form.js
+++ js/form.js
@@ -7,16 +7,12 @@
         let correo;
         $select.addEventListener('change', () => {
             let valorOption = $select.value;
-            // console.log(valorOption);
             var optionSelect = $select.options[$select.selectedIndex];
-            // console.log("Opción:", optionSelect.text);
-            // console.log("Valor:", optionSelect.value);
             correo = optionSelect.value;
             let  inputResult = d.querySelector('#resultado').value=(optionSelect.value)
           });
     $form.addEventListener("submit", (e) => {
         e.preventDefault();
-        // console.log(correo);
         $loader.classList.remove("none");
         fetch(`https://formsubmit.co/ajax/${correo}`, {
             method: "POST",
@@ -24,11 +20,9 @@
         })
             .then((res) => (res.ok ? res.json() : Promise.reject(res)))
             .then((json) => {
-                // console.log(json);
                 location.hash = "#gracias";
                 $form.reset();
             }).catch((err) => {
-                // console.log(err);
                 let message = err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
                 $response.querySelector("h3").innerHTML = `Erro ${err.status}:${message}`;
             }).finally(() => {
